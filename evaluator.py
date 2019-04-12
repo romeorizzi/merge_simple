acd70a9c8b4de2a9871adef42f0cade37dd58ade
alerighi <alessandro.righi@outlook.it>
2019-04-12 10:25:16 +0200

    Finito problema sul merge

--- evaluator.py
+++ evaluator.py
@@ -2,45 +2,43 @@ import random
 import turingarena as ta
 
 def test_case(A, B, C):
-    perm = range(len(C))
-    random.shuffle(perm)
+    print(f"testing case len(A) = {len(A)}, len(B) = {len(B)}\t", end="")
     with ta.run_algorithm(ta.submission.source) as p:
-        p.functions.get_two_sorted_lists_and_store_their_merge(len(A), A, len(B), B)
-    except ta.AlgorithmError as e:
-        print(f" error: {e}")
-        ta.goals.setdefault("correct", False)
-        ta.goals.setdefault("linear_merge", False)
-        ta.goals.setdefault("correct_YES_certificate", False)
+        try:
+            p.procedures.merge(len(A), A, len(B), B, len(C))
+            
+            res = [p.functions.getElement(i) for i in range(len(C))]
+            
+            for i in range(len(C)):
+                
+                if res[i] != C[i]:
+                    print(f"\nWrong merge at element {i}: expected {C[i]}, found {res[i]}")
+                    if len(C) <= 20:
+                        print(f"A array = {A}")
+                        print(f"B array = {B}")
+                        print(f"Your merge = {res}")
+                        print(f"Correct merge = {C}")
+                    return False
 
-    for pi in perm:
-        with ta.run_algorithm(ta.submission.source) as p:
-            res = p.functions.tell_element_in_pos(pos)
         except ta.AlgorithmError as e:
-            print(f" error: {e}")
-            ta.goals.setdefault("correct", False)
-            ta.goals.setdefault("linear_merge", False)
-            ta.goals.setdefault("correct_YES_certificate", False)
-        if res != C[pi]:
-            ta.goals.setdefault("correct", False)
-            ta.goals.setdefault("linear_merge", False)
-            ta.goals.setdefault("correct_YES_certificate", False)
-            print("Il numero che avevo in mente era %d, non %d come da tè affermato." % (N,res) )
-
-
+            print(f"\nError: {e}")
+            return False
+    print("CORRECT")
+    return True
+       
 def run_all_test_cases():
-    C = range(1,1000000,1000)
-    A = []
-    B = []
-    for c in C:
-        if random.randint(0,1) == 1:
-            A.append(c)
-        else:
-            B.append(c)
-        test_case(A, B, C)
+    cases = (10, 20, 50, 100, 1000, 10000)
+    for c in cases:
+        C = sorted([random.randint(0, 1000) for _ in range(c)])
+        A = []
+        B = []
+        for x in C:
+            if random.randint(0,1) == 1:
+                A.append(x)
+            else:
+                B.append(x)
+        ta.goals.check("correct", test_case, A, B, C)
         
 run_all_test_cases()        
 
 ta.goals.setdefault("correct", True)
-ta.goals.setdefault("linear_merge", True)
-ta.goals.setdefault("direct_access", True)
-print(ta.goals)
